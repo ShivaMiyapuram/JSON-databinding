5dc96b5337e5ed1791d194d4aa4d7fca86d6b026
shivateja <shivateja219@gmail.com>
2018-07-05 18:58:37 -0400

    added databinding code for JSON to POJO conversion

--- src/main/java/com/luv2code/jasckson/json/demo/Student.java
+++ src/main/java/com/luv2code/jasckson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package com.luv2code.jasckson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
 	private int id;
